12a538ae63d67c2ec6107befec811d18f0ed7144
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-08-27 04:18:33 +0600

    done

--- tohoney_E/products/urls.py
+++ tohoney_E/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from products.views import product,single_product
 urlpatterns = [
     path('',product , name='products'),
-    path('single_product/',single_product , name='single_products')
+    path('single_product/',single_product , name='single_products'),
+    path('category/<slug:category_slug>/',product, name='products_by_category'),
 ]
--- tohoney_E/products/views.py
+++ tohoney_E/products/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from products.models import ProductModel
+from category.models import CategoryModel
 
 # Create your views here.
-def product(request):
-    return render(request,'products/shop.html')
+def product(request,category_slug=None):
+    categories=CategoryModel.objects.all()
+    if category_slug is not None:
+        category=get_object_or_404(CategoryModel,slug=category_slug)
+        products=ProductModel.objects.filter(is_available=True,category=category)
+    else:
+        category=None
+        products=ProductModel.objects.filter(is_available=True)
+    return render(request,'products/shop.html',context={'products':products,'categories':categories})
 
 def single_product(request):
     return render(request,'products/single-product.html')
