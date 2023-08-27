09aa7ce389de898ef0a644860c3b5ba7537f84e3
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-08-28 01:54:46 +0600

    done

--- tohoney_E/cart/urls.py
+++ tohoney_E/cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from cart.views import cart,checkout
+from cart.views import cartview,checkout,add_to_cart
 
 urlpatterns = [
-    path('',cart,name='cart'),
+    path('',cartview,name='cart'),
+    path('<int:product_id>/',add_to_cart,name='add_to_cart'),
     path('checkout/',checkout,name='checkout')
 ]
--- tohoney_E/cart/views.py
+++ tohoney_E/cart/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from products.models import ProductModel
+from cart.models import CartModel,CartItemModel
 
 # Create your views here.
-def cart(request):
+def cartview(request):
     return render(request,'cart/cart.html')
+def add_to_cart(request,product_id):
+    product=ProductModel.objects.get(id=product_id)
+    session_id=request.session.session_key
+    cart_id=CartModel.objects.filter(cart_id=session_id).exists()
+    if cart_id :
+        cart_item=CartItemModel.objects.filter(product=product_id).exists()
+        if cart_item:
+            item=CartItemModel.objects.get(product=product)
+            item.quantity+=1
+            item.save()
+        else:
+            cart_id=CartModel.objects.get(cart_id=session_id)
+            item=CartItemModel.objects.create(
+                cart=cart_id,
+                product=product,
+                quantity=1
+            )
+    else:
+        cart=CartModel.objects.create(cart_id=session_id)
+        cart.save()
+    return redirect('cart')
+    
 def checkout(request):
     return render(request,'cart/checkout.html')
 
